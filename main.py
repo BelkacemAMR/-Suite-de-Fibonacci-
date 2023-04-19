fa4a6522e2c19c3df592369d9b00368e2c6868b7
Belkacem <abel-and-co@hotmail.com>
2023-04-19 09:44:07 +0200

    Version initiale

---
+++ main.py
@@ -0,0 +1,13 @@
+if (n == 0):
+    return []
+if (n == 1):
+    return [1]
+'''if (n == 2):
+   return[1,1]'''
+fibonacci = [0] * (n)
+fibonacci[0] = 1
+fibonacci[1] = 1
+for i in range(2, n):
+    fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2]
+
+return fibonacci
